7481e0ba7495425ab6671ef0e04c0122bdaab57e
medvehagyma <bb.eniko99@gmail.com>
2026-02-14 13:33:13 +0100

    add counting example about temperatures with tests

---
+++ programming_patterns/counting/temperature/test_temperature.py
@@ -0,0 +1,31 @@
+import pytest
+from temperature import cntHotterThan28 
+
+
+# run the tester with the following command:
+# py -m pytest test_temperature.py
+
+
+# alapvető használat tesztelése
+
+def test_happyPath () :
+
+    temperatures = [23, 26.3, 29, 36, 37.6, 39]
+
+    assert cntHotterThan28 (temperatures) == 4
+
+
+
+def test_emptyList () :
+
+    temperatures = []
+
+    assert cntHotterThan28 (temperatures) == 0
+
+
+
+def test_tresholdValueCounted () :
+
+    temperatures = [23, 26.3, 28, 29, 36, 37.6, 39]
+
+    assert cntHotterThan28 (temperatures) == 4
