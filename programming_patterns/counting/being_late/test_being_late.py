adb217425be846c74db4c2f998466fda86ba3681
medvehagyma <bb.eniko99@gmail.com>
2026-02-16 13:30:54 +0100

    completed counting homework being late

--- programming_patterns/counting/being_late/test_being_late.py
+++ programming_patterns/counting/being_late/test_being_late.py
@@ -4,3 +4,23 @@ from being_late import countMin
 
 # test basic use
 
+def test_happyPath () :
+    minutes = [4, 6, 0, 13, 16, 0, 3, 0, 8, 17]
+    assert countMin (minutes) == 2
+
+def test_emptyList () :
+    minutes = []
+    assert countMin (minutes) == 0
+
+def test_allUnderThreshold () :
+    minutes = [4, 6, 0, 13, 14, 0, 3, 0, 8, 12]
+    assert countMin (minutes) == 0
+
+
+def test_allAboveThreshold () :
+    minutes = [24, 16, 20, 23, 16, 20, 23, 20, 18, 17]
+    assert countMin (minutes) == 10
+
+def test_tresholdValuePresent () :
+    minutes = [4, 6, 0, 13, 15, 16, 0, 3, 0, 8, 17]
+    assert countMin (minutes) == 2
