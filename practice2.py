82e498dd93c3e5e4004fdda4414a87b9d00333d5
Falesz <jnjwrg@inf.elte.hu>
2026-03-13 05:18:11 +0100

    Add detailed comments to more practice files

--- practice2.py
+++ practice2.py
@@ -1,6 +1,9 @@
 def greeting(name):
     print('Hello '+name+'!')
 
+# The parameter values of functions can also be many different types. Values enclosed by "" or '' characters are strings. These are text values.
 greeting('Ádám')
-stringNum = "5"
-realNum = 5
+
+# It is important to distinguish strings that represent numbers and actual numbers.
+stringNum = "5" # This is a text that looks like the number 5. It can not be used for arithmetics.
+realNum = 5 # This is an actual 5.
